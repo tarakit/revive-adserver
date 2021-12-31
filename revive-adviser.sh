44706fbaaec9f5bd6f88a74486a4c3d1a3bf0e34
tarakit <darakit069@gmail.com>
2021-12-31 08:50:16 +0000

    update nginx and add cerbot

--- revive-adviser.sh
+++ revive-adviser.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # build an image from Dockerfile
+sudo docker container rm -f revive-adserver-sql revive-adserver
+sudo docker rmi revive-adserver --force
+
 docker build -t revive-adserver .
 
-docker run -d --name revive-adviser -p 80:80 revive-adviser
+docker run -d --name revive-adserver -p 8000:80 revive-adserver
 echo "=========================="
 echo "===== Wait a moment ======"
 echo "=========================="
@@ -21,6 +24,16 @@ docker run \
 --volume=/root/docker/revive-adserver-sql/conf.d:/etc/mysql/conf.d \
 --volume=$MOUNTING:/var/lib/mysql \
 mysql
+
+# Install Certbot
+# sudo apt install certbot python3-certbot-nginx
+sudo cp nginx/ads.conf /etc/nginx/sites-available/ads.golpages-cambodia.conf
+sudo ln -s /etc/nginx/sites-available/ads.golpages-cambodia.conf /etc/nginx/sites-enabled/
+sudo nginx -t
+sudo systemctl restart nginx
+sleep 5s
+sudo certbot --nginx -d ads.golpages-cambodia.com
+sudo systemctl reload nginx
 
 echo "=========================="
 echo "===== Done!! ======"
